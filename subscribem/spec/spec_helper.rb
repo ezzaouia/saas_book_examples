844676bd903a82b6cf5c8e8744c0ae240e53f291
Jefferson Queiroz Venerando <jefferson@shamanime.com>
2013-08-12 22:53:27 -0300

    Fixes a typo when including Subscribem::TestingSupport::DatabaseCleaning
    Added a missing 'g' when including `Subscribem::TestingSupport::DatabaseCleaning` in the spec_helper.rb

--- subscribem/spec/spec_helper.rb
+++ subscribem/spec/spec_helper.rb
@@ -16,7 +16,7 @@ Dir[File.dirname(__FILE__) + "/support/**/*.rb"].each {|f| require f}
 ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
 
 RSpec.configure do |config|
-  config.include Subscribem::TestingSupport::DatabaseCleanin
+  config.include Subscribem::TestingSupport::DatabaseCleaning
   # ## Mock Framework
   #
   # If you prefer to use mocha, flexmock or RR, uncomment the appropriate line:
